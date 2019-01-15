93791a4aec2456d3a07f30b66a6d40238db5fc8b
Do Duy <juchiast@gmail.com>
2019-01-15 20:59:15 +0700

    .

--- app/src/main/java/andro/id/caroboard/CaroBoardFragment.java
+++ app/src/main/java/andro/id/caroboard/CaroBoardFragment.java
@@ -1,14 +1,28 @@
 package andro.id.caroboard;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class CaroBoardFragment extends Fragment {
+    MainActivity main;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.caro_board_layout, container, false);
+        View view = inflater.inflate(R.layout.caro_board_layout, container, false);
+        main.boardReady(view);
+        return view;
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        main = (MainActivity) context;
     }
 }
--- app/src/main/java/andro/id/caroboard/MenuFragment.java
+++ app/src/main/java/andro/id/caroboard/MenuFragment.java
@@ -1,6 +1,8 @@
 package andro.id.caroboard;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.support.constraint.ConstraintLayout;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -10,5 +12,10 @@ public class MenuFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         return inflater.inflate(R.layout.menu_layout, container, false);
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
     }
 }
