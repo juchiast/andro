b6bb412bcaca18f5fb002050f23fd0a7e3983a88
Do Duy <juchiast@gmail.com>
2019-01-15 17:17:08 +0700

    sticky

--- app/src/main/java/andro/id/caroboard/MainActivity.java
+++ app/src/main/java/andro/id/caroboard/MainActivity.java
@@ -73,7 +73,7 @@ public class MainActivity extends AppCompatActivity {
         // Or for "sticky immersive," replace it with SYSTEM_UI_FLAG_IMMERSIVE_STICKY
         View decorView = getWindow().getDecorView();
         decorView.setSystemUiVisibility(
-                          View.SYSTEM_UI_FLAG_IMMERSIVE
+                          View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
                         | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
                         | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
                         | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
